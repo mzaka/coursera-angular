a5b9d675fccb5e12d67c6c202f96eb3725a5f82e
zakauddin <m.zaka7@gmail.com>
2017-12-07 00:55:08 +0400

    Loading

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -7,14 +7,25 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
-  }
-  getDish(id: number): Dish {
-    return DISHES.filter((dish) => (dish.id === id))[0];
+  getDishes(): Promise<Dish[]> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES), 2000);
+    });
   }
 
-  getFeaturedDish(): Dish {
-    return DISHES.filter((dish) => dish.featured)[0];
+  getDish(id: number): Promise<Dish> {
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    });
   }
+
+  getFeaturedDish(): Promise<Dish> {
+    return  new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+    });
+  }
+
 }
